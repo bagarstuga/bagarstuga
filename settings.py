a967f0d6881fd296a4b363e79f09851ab176736f
3xer <57897265+3xer@users.noreply.github.com>
2020-04-02 16:29:11 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,26 @@
 TITLE = "SharkRush"
+
+# specs
 WIDTH = 800
 HEIGHT = 600
 FPS = 60
+
+# colours
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-PLAYER_FRICTION = -0.12
-PLAYER_ACC = 0.5
+
+# fysics values 
+shark_FRICTION = -0.12
+shark_ACC = 0.5
+shark_gravity = -0.3
+
+# ground list
+# w = Width
+# h = Height
+ground_list = [(x, y, w, h)]
+
+
