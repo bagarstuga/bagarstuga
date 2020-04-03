c39b0de5325ea4ef45a9a8546ab10a88fe227cc6
3xer <57897265+3xer@users.noreply.github.com>
2020-04-03 10:08:13 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,13 +14,11 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
 # fysics values 
-shark_FRICTION = -0.12
-shark_ACC = 0.5
-shark_gravity = -0.3
+shark_Fiction = -0.12
+shark_Acceleration= 0.5
+shark_Gravity = -0.3
 
 # ground list
 # w = Width
 # h = Height
-ground_list = [(x, y, w, h)]
-
-
+Ground_list = [(x, y, w, h)]
