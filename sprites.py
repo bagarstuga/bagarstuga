d5dabff57a7b9f83c79388da3b000024c2247c49
Adrian <bolebrallorna@gmail.com>
2020-04-08 14:23:45 +0200

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -3,6 +3,5 @@ from settings import *
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
-        # pg.sprite.Sprite.sur
         self.image= pg.Surface((60,30))
         self.image.load("shark-facing-right.png")
