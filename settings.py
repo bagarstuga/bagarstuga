0ec409c99afad23d76dce303ce41ebe2f98d3595
page222222 <55085361+page222222@users.noreply.github.com>
2020-04-08 10:08:08 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,24 +1,6 @@
-TITLE = "SharkRush"
-
-# specs
-WIDTH = 800
-HEIGHT = 600
-FPS = 60
-
-# colours
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+"""TODO"""
+SCR_WIDTH = 400
+SCR_HEIGHT = 600
+TITLE = "Shark Rush"
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-
-# fysics values 
-shark_Fiction = -0.12
-shark_Acceleration= 0.5
-shark_Gravity = -0.3
-
-# ground list
-# w = Width
-# h = Height
-Ground_list = [(x, y, w, h)]
+BLACK = (255, 255, 255)
