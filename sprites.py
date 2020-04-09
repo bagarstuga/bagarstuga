b4fe89687b7066517a5143ce116b43f4c550d869
page222222 <55085361+page222222@users.noreply.github.com>
2020-04-09 15:50:02 +0200

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,7 +1,8 @@
-import pygame as pg
-from settings import *
+#import pygame as pg
+#from settings import *
 
-class Player(pg.sprite.Sprite):
-    def __init__(self):
-        self.image= pg.Surface((60,30))
-        self.image.load("shark-facing-right.png")
+#class Player(pg.sprite.Sprite):
+#    def __init__(self):
+#        self.image= pg.Surface((60,30))
+#        self.image.load("shark-facing-right.png")
+#Utkommenterad då det inte används för tillfället och eller inte är klart
