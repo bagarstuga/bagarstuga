270285becc4d10ed93e3061129f9607e5b0bf0e0
3xer <57897265+3xer@users.noreply.github.com>
2020-04-09 15:52:59 +0200

    Add files via upload

--- fysik.py
+++ fysik.py
@@ -1,5 +1,21 @@
 # Sharkrush
 
-#def gavity(self):
-#    self.speedY += 3:
-#Utkommenterat då det inte används och eller inte är klart för tillfället
+import pygame as pg
+#vel = velocity Y = Yaxis
+def gavity(self):
+    self.velY += 3 #falling speed
+
+    if self.surface.y > mapY and self.vely >= 0:
+        self.mapY = 0
+        self.surface.y = mapY-ty-ty
+
+"""TODO"""
+class Player(pg.sprite.Sprite)
+# player
+    def __init__(self):
+        pass
+
+"""TODO"""
+class Ground(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pass
