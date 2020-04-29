e91e63991cd3ef59b5ad130f7a9a633d8ec847db
page222222 <55085361+page222222@users.noreply.github.com>
2020-04-29 10:00:12 +0200

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,8 +1,24 @@
-#import pygame as pg
-#from settings import *
+"""TODO"""
+import pygame as pg
+from settings import *
 
-#class Player(pg.sprite.Sprite):
-#    def __init__(self):
-#        self.image= pg.Surface((60,30))
-#        self.image.load("shark-facing-right.png")
-#Utkommenterad då det inte används för tillfället och eller inte är klart
+class shark(pg.sprite.Sprite):
+    """TODO"""
+    def __init__(self):
+        """TODO"""
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((30, 40))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (SCR_WIDTH / 2, SCR_HEIGHT / 2)
+        self.vx = 0
+        self.vy = 0
+    def update(self):
+        """TODO"""
+        key_press = pg.key.get_pressed()
+        if key_press[pg.K_LEFT]:
+            self.vx = -3
+        if key_press[pg.K_RIGHT]:
+            self.vx = 3
+        self.rect.x += self.vx
+        self.rect.y += self.vy
