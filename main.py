1446ca57bd384dfe6eaa6d1152ae7e3b4fe6b1a7
page222222 <55085361+page222222@users.noreply.github.com>
2020-04-30 12:10:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 """TODO"""
-import pygame as pg
 import random
+import pygame as pg
 from sprites import *
 from settings import *
 
-class main_class:
+class MainClass:
     """TODO"""
     def __init__(self):
         """TODO"""
@@ -33,10 +33,8 @@ class main_class:
             self.go()
             self.graphic()
             self.update()
+            self.end_game()
             pg.display.flip()
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.running = False
         pg.quit()
     def graphic(self):
         """TODO"""
@@ -45,16 +43,42 @@ class main_class:
     def end_screen(self):
         """TODO"""
         pass
+    def end_game(self):
+        """TODO"""
+        for event in pg.event.get():
+             if event.type == pg.QUIT:
+                 self.running = False
+                 self.done = True
     def start_screen(self):
         """TODO"""
-        pass
-
+        done = False
+        font1 = pg.font.Font("freesansbold.ttf", 32)
+        font2 = pg.font.Font("freesansbold.ttf", 20)
+        text1 = font1.render("SharkRush", True, WHITE, BLUE)
+        text_rect1 = text1.get_rect()
+        text_rect1.center = (SCR_WIDTH / 2, SCR_HEIGHT / 2)
+        text2 = font2.render("Press any key to continue", True, GREY, BLUE)
+        text_rect2 = text2.get_rect()
+        text_rect2.center = (SCR_WIDTH / 2, SCR_HEIGHT/ 1.8)
+        while not done:
+            self.screen.fill(BLUE)
+            self.screen.blit(text1, text_rect1)
+            self.screen.blit(text2, text_rect2)
+            pg.display.update()
+            for event in pg.event.get():
+                if event.type == pg.KEYUP:
+                    done = True
+                elif event.type == pg.QUIT:
+                    pg.quit()
+                    quit()
+        self.render()
 def main():
     """TODO"""
-    m = main_class()
+    m = MainClass()
     m.start_screen()
-    m.render()
+    
 
 if __name__== "__main__":
     main()
+
 
