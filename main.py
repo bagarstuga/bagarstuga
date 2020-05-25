c194436f2a9a3e52096ef1fdfb74b19b18f67e81
page222222 <55085361+page222222@users.noreply.github.com>
2020-05-25 11:48:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """The main module for the game SharkRush. Run this in order to start the game"""
-import random
 import pygame as pg
-from sprites import *
+from shark import *
 from settings import *
+from platforms import Platform
 
 class MainClass:
     """This class contains methods for the start-screen,
@@ -13,22 +13,29 @@ class MainClass:
         pg.mixer.init()
         self.screen = pg.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
         pg.display.set_caption(TITLE)
+        self.sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Shark()
         self.timing = pg.time.Clock()
         self.running = True
     def render(self):
         """When the game starts, create these first and play music,
            then go to the game loop in events(). Here the class Shark() is
            imported from sprites and becomes the player variable"""
-        self.sprites = pg.sprite.Group()
-        self.player = Shark()
         self.sprites.add(self.player)
-        pg.mixer.music.load("FadeNCS.mp3")
+        pg.mixer.music.load("FadeNCS.ogg")
         pg.mixer.music.play(-1)
+        ground = Platform(0, SCR_HEIGHT/1.8, SCR_WIDTH, 30)
+        self.sprites.add(ground)
+        self.platforms.add(ground)
         self.events()
     def update(self):
         """Updates the sprites and updates the screen"""
         self.sprites.update()
-        pg.display.update()
+        collide = pg.sprite.spritecollide(self.player, self.platforms, False)
+        if collide:
+            self.player.pos.y = collide[0].rect.top
+            self.player.vel.y = 0
     def events(self):
         """The game loop where the other methods are called and run
            until the end_game() method is called and running is false
@@ -38,29 +45,28 @@ class MainClass:
             self.timing.tick(FPS)
             self.graphic()
             self.update()
-            self.end_game()
+            self.jumping_end_game()
             pg.display.flip()
         pg.quit()
     def graphic(self):
         """Draws the different sprites and the background"""
         background_img = pg.transform.scale\
-        (pg.image.load("pool-water.jpg"),(SCR_WIDTH, SCR_HEIGHT))
+        (pg.image.load("pool-water.jpg"), (SCR_WIDTH, SCR_HEIGHT))
         self.screen.blit(background_img, (0, 0))
         self.sprites.draw(self.screen)
-    def end_screen(self):
+    def jumping_end_game(self):
         """This method will contain the end screen"""
-        pass
-    def end_game(self):
-        """A loop that checks if an event is the QUIT event, if so
-           running is false, this stops the game loop"""
         for event in pg.event.get():
-             if event.type == pg.QUIT:
-                 self.running = False
+            if event.type == pg.QUIT:
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
     def start_screen(self):
         """This method defines two fonts, used for the two different
            text lines, these are put on top of the background,
            then checks if any key is pressed, if so, go to render(),
-           otherwise, quit. There is a number 1.8 here, this is just a number
+           otherwise, quit. There is a number 50 here, this is just a number
            which places the second line of text in a good place, right below
            the first line"""
         done = False
@@ -71,7 +77,7 @@ class MainClass:
         text_rect1.center = (SCR_WIDTH / 2, SCR_HEIGHT / 2)
         text2 = font2.render("Press any key to continue", True, GREY, BLUE)
         text_rect2 = text2.get_rect()
-        text_rect2.center = (SCR_WIDTH / 2, SCR_HEIGHT/ 1.8)
+        text_rect2.center = (SCR_WIDTH / 2, SCR_HEIGHT/ 2 + 50)
         while not done:
             self.screen.fill(BLUE)
             self.screen.blit(text1, text_rect1)
@@ -79,7 +85,69 @@ class MainClass:
             pg.display.update()
             for event in pg.event.get():
                 if event.type == pg.KEYUP:
-                    done = True
+                    done = True"""The main module for the game SharkRush. Run this in order to start the game"""
+import pygame as pg
+from shark import *
+from settings import *
+from platforms import Platform
+
+class MainClass:
+    """This class contains methods for the start-screen,
+       updating the screen, drawing sprites and the game loop"""
+    def __init__(self):
+        """TODO"""
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Shark()
+        self.timing = pg.time.Clock()
+        self.running = True
+    def render(self):
+        """When the game starts, create these first and play music,
+           then go to the game loop in events(). Here the class Shark() is
+           imported from sprites and becomes the player variable"""
+        self.sprites.add(self.player)
+        pg.mixer.music.load("FadeNCS.ogg")
+        pg.mixer.music.play(-1)
+        ground = Platform(0, SCR_HEIGHT/1.8, SCR_WIDTH, 30)
+        self.sprites.add(ground)
+        self.platforms.add(ground)
+        self.events()
+    def update(self):
+        """Updates the sprites and updates the screen"""
+        self.sprites.update()
+        collide = pg.sprite.spritecollide(self.player, self.platforms, False)
+        if collide:
+            self.player.pos.y = collide[0].rect.top
+            self.player.vel.y = 0
+    def events(self):
+        """The game loop where the other methods are called and run
+           until the end_game() method is called and running is false
+           here the updating speed is set to the constant
+           FPS"""
+        while self.running:
+            self.timing.tick(FPS)
+            self.graphic()
+            self.update()
+            self.jumping_end_game()
+            pg.display.flip()
+        pg.quit()
+    def graphic(self):
+        """Draws the different sprites and the background"""
+        background_img = pg.transform.scale\
+        (pg.image.load("pool-water.jpg"), (SCR_WIDTH, SCR_HEIGHT))
+        self.screen.blit(background_img, (0, 0))
+        self.sprites.draw(self.screen)
+    def jumping_end_game(self):
+        """This method will contain the end screen"""
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
                 elif event.type == pg.QUIT:
                     pg.quit()
                     quit()
@@ -88,8 +156,7 @@ def main():
     """This function runs the method start_screen to display the start screen
        it is used to prevent global variables.
        """
-    m = MainClass()
-    m.start_screen()
-if __name__== "__main__":
+    MainClass().start_screen()
+if __name__ == "__main__":
     main()
 
