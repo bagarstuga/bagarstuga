073ac72e25ad56791fbf885dbdc5b9e470e39793
page222222 <55085361+page222222@users.noreply.github.com>
2020-05-27 15:33:04 +0200

    Update platforms.py

--- platforms.py
+++ platforms.py
@@ -3,9 +3,12 @@
 import pygame as pg
 from settings import *
 class Platform(pg.sprite.Sprite):
-    """The class which contain the init method that
-       puts these parameters in place when initialized"""
+    """A class for how the platforms are to behave."""
     def __init__(self, x_pos, y_pos, width, height):
+        """The platformns are created with its postition,
+           height and width. It gets a color and the postition
+           is specified. It also makes the class a sprite in
+           pygame"""
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((width, height))
         self.image.fill(RED)
